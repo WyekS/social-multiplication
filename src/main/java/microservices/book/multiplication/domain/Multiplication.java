ae819e1541f923367e09d28531d590ef3e21a676
irufian <irufian@hiberus.com>
2020-01-09 20:04:23 +0100

    [master] Initial commit - practica 6.02

---
+++ src/main/java/microservices/book/multiplication/domain/Multiplication.java
@@ -0,0 +1,30 @@
+package microservices.book.multiplication.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+/**
+ * This represents a Multiplication (a * b).
+ */
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
+
+	// Both factors
+	private final int factorA;
+	private final int factorB;
+
+	// Empty constructor for JSON (de)serialization
+	Multiplication() {
+		this(0, 0);
+	}
+
+	public int getFactorA() {
+		return this.factorA;
+	}
+
+	public int getFactorB() {
+		return this.factorB;
+	}
+}
