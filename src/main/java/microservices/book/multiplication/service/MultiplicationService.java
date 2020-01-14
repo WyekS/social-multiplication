670681684612c46c671adeacfb8b4d37d8edd254
Ismael <wyeks@live.com>
2020-01-14 16:23:18 +0100

    [transicion_arquitectura] - Initial commit transicion

--- src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -6,18 +6,34 @@ import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import java.util.List;
 
 public interface MultiplicationService {
-	/**
-	 * Generates a random {@link Multiplication} object.
-	 *
-	 * @return a multiplication of randomly generated numbers
-	 */
-	Multiplication createRandomMultiplication();
+    /**
+     * Generates a random {@link Multiplication} object.
+     *
+     * @return a multiplication of randomly generated numbers
+     */
+    Multiplication createRandomMultiplication();
 
-	/**
-	 * @return true if the attempt matches the result of the
-	 * multiplication, false otherwise.
-	 */
-	boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
+    /**
+     * @return true if the attempt matches the result of the
+     * multiplication, false otherwise.
+     */
+    boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
 
-	List<MultiplicationResultAttempt> getStatsForUser(String userAlias);
+    /**
+     * Gets the statistics for a given user.
+     *
+     * @param userAlias the user's alias
+     * @return a list of {@link MultiplicationResultAttempt} objects, being the past attempts of the user.
+     */
+
+    List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
+
+    /**
+     * Gets an attempt by its id
+     *
+     * @param resultId the identifier of the attempt
+     * @return the {@link MultiplicationResultAttempt} object matching the id, otherwise null.
+     */
+    MultiplicationResultAttempt getResultById(final Long resultId);
+
 }
