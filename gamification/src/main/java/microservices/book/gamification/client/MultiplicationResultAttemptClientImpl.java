3c90345b4e9f5ff92bca16254bab4b73cf2023d7
Ismael <wyeks@live.com>
2020-01-15 19:13:09 +0100

    [practica-9.01] - final commit practica 9 - REMOVE webconfiguration file - not necessary

--- gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
+++ gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -1,5 +1,6 @@
 package microservices.book.gamification.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import microservices.book.gamification.client.dto.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,18 +13,24 @@ import org.springframework.web.client.RestTemplate;
  */
 @Component
 class MultiplicationResultAttemptClientImpl implements MultiplicationResultAttemptClient {
-	private final RestTemplate restTemplate;
-	private final String multiplicationHost;
+    private final RestTemplate restTemplate;
+    private final String multiplicationHost;
 
-	@Autowired
-	public MultiplicationResultAttemptClientImpl(final RestTemplate restTemplate, @Value("${multiplicationHost}") final String multiplicationHost) {
-		this.restTemplate = restTemplate;
-		this.multiplicationHost = multiplicationHost;
-	}
+    @Autowired
+    public MultiplicationResultAttemptClientImpl(final
+                                                 RestTemplate restTemplate, @Value("${multiplicationHost}") final String multiplicationHost) {
+        this.restTemplate = restTemplate;
+        this.multiplicationHost = multiplicationHost;
+    }
 
-	@Override
-	public MultiplicationResultAttempt retrieveMultiplicationResultAttemptbyId(final Long multiplicationResultAttemptId) {
-		return restTemplate.getForObject(multiplicationHost + "/results/" + multiplicationResultAttemptId, MultiplicationResultAttempt.class);
-	}
+    @HystrixCommand(fallbackMethod = "defaultResult")
+    @Override
+    public MultiplicationResultAttempt retrieveMultiplicationResultAttemptbyId(final Long multiplicationResultAttemptId) {
+        return restTemplate.getForObject(multiplicationHost + "/results/" + multiplicationResultAttemptId, MultiplicationResultAttempt.class);
+    }
+
+    private MultiplicationResultAttempt defaultResult(final Long multiplicationResultAttemptId) {
+        System.out.println("Executing defaultResult() method");
+        return new MultiplicationResultAttempt("fakeAlias", 10, 10, 100, true);
+    }
 }
-
--- gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
+++ gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
@@ -1,9 +1,11 @@
 package microservices.book.gateway;
 
+import microservices.book.gateway.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +14,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @EnableEurekaClient
 @EnableZuulProxy
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
